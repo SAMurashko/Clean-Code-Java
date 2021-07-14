11379e765f02bb5e5cfa74fa4cfdb9ff5c739cb0
SAMurashko <stas9n.omg@mail.ru>
2021-07-14 14:50:05 +0300

    Clean code task

---
+++ Aircompany-Java/src/main/java/Planes/Plane.java
@@ -0,0 +1,54 @@
+package Planes;
+
+import java.util.Objects;
+
+abstract public class Plane {
+    String model;
+    private int maxSpeed;
+    private int maxFlightDistance;
+    private int maxLoadCapacity;
+
+    public Plane(String model, int maxSpeed, int maxFlightDistance, int maxLoadCapacity) {
+        this.model = model;
+        this.maxSpeed = maxSpeed;
+        this.maxFlightDistance = maxFlightDistance;
+        this.maxLoadCapacity = maxLoadCapacity;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getMS() {
+        return maxSpeed;
+    }
+
+    public int Get_Max_Flight_Distance() {
+        return maxFlightDistance;
+    }
+
+    public int getMinLoadCapacity() {
+        int result = this.maxLoadCapacity;
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Plane{" +
+                "model='" + model + '\'' +
+                ", maxSpeed=" + maxSpeed +
+                ", maxFlightDistance=" + maxFlightDistance +
+                ", maxLoadCapacity=" + maxLoadCapacity +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return (!(o instanceof Plane));
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(model, maxSpeed, maxFlightDistance, maxLoadCapacity);
+    }
+}
